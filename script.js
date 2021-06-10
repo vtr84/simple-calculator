b4672a36bfd189b9c2fcc5ccf86f507893211a3a
Victor Isidoro <victorisidoro@icloud.com>
2021-06-10 11:33:33 +0100

    New script file created

---
+++ script.js
@@ -0,0 +1,46 @@
+        // const firstPlayer = prompt(`🤖\nHey, I'm a bot..\nWhat's your name?`)
+        // const rndNumber = Math.floor(Math.random()*100)+1
+        // let botWinning = true
+        // //let hint = ''
+        // //console.log(rndNumber)
+
+        // let guess = parseInt(prompt(`🎰\nLet's play a game!\nGuess the number I'm thinking between 1 and 100 and win big! 💰`))
+
+        // while (botWinning) {
+        //     if (rndNumber === guess){
+        //         console.log(`Oh Uau ${firstPlayer}, you won!\nHere's some virtual money.. 💰💰💰💰💰\nEnjoy!`)
+        //         botWinning = false;
+        //     } else {
+        //         if (rndNumber < guess) {
+        //             console.log('The number is Less ⬇️')
+        //             // hint = 'The number is Less ⬇️'
+        //         } else {
+        //             console.log('The number is More ⬆️')
+        //             // hint = 'The number is More ⬆️'
+        //         }
+        //         guess = parseInt(prompt(`Sorry, wrong number! Try again:\n💡: check console`))
+        //     }
+        // }
+
+let firstValue = parseInt(prompt('💁‍♂️ \nPlease enter the first value:'))
+let operator = prompt('💁‍♂️ \nPlease enter the operation you wish to perfom (+, -, /, *):')
+let secondValue = parseInt(prompt('💁‍♂️\nPlease enter the second value:'))
+
+switch (operator){
+    case '+':
+        console.log(firstValue + secondValue)
+        break;
+    case '-':
+        console.log(firstValue - secondValue)
+        break;
+    case '/':
+        console.log(firstValue / secondValue)
+        break;
+    case '*':
+        console.log(firstValue * secondValue)
+        break;
+    default :
+        console.log('Invalid Operator')
+}
+
+       
